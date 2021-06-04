f39a90055fb4f297c168324e89eaf4d6efa45776
jefferson-norberto <jefferson.norbertto@gmail.com>
2021-06-04 15:54:36 -0300

    Implementation Constants filter and click event in MainActivity.kt

--- app/src/main/java/com/norbertto/motivation/infra/MotivationConstats.kt
+++ app/src/main/java/com/norbertto/motivation/infra/MotivationConstats.kt
@@ -9,4 +9,10 @@ class MotivationConstats private constructor(){
     object KEY{
         val PERSON_NAME = "name"
     }
+
+    object PHRASEFILTER{
+        val ALL = 1
+        val HAPPY = 2
+        val MORNING = 3
+    }
 }
--- app/src/main/java/com/norbertto/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/norbertto/motivation/ui/MainActivity.kt
@@ -1,19 +1,69 @@
- package com.norbertto.motivation.ui
+package com.norbertto.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.norbertto.motivation.R
 import com.norbertto.motivation.infra.MotivationConstats
 import com.norbertto.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
- class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity(), View.OnClickListener {
      private lateinit var mSecurityPreferences: SecurityPreferences
 
      override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mSecurityPreferences = SecurityPreferences(this)
-        textName.text = mSecurityPreferences.getString(MotivationConstats.KEY.PERSON_NAME)
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+
+         //recuperando o nome do usuário pelo sharedPreferences
+         mSecurityPreferences = SecurityPreferences(this)
+         textName.text = mSecurityPreferences.getString(MotivationConstats.KEY.PERSON_NAME)
+
+         hideActionBar()
+
+         buttonNewPhrase.setOnClickListener(this)
+         imageAll.setOnClickListener(this)
+         imageHappy.setOnClickListener(this)
+         imageMorning.setOnClickListener(this)
     }
-}
+
+     private fun hideActionBar() {
+         if(supportActionBar != null){
+             supportActionBar!!.hide()
+         }
+     }
+
+     override fun onClick(view: View?) {
+         val id = view?.id
+         val listFilter = listOf(R.id.imageAll, R.id.imageHappy, R.id.imageMorning)
+
+         if (id == R.id.buttonNewPhrase){
+            handleNewPhrase()
+         }else if(id in listFilter){
+             handleFilter(id!!)
+         }
+     }
+
+     private fun handleNewPhrase(){
+
+     }
+
+     private fun handleFilter(id: Int){
+         imageAll.setColorFilter(resources.getColor(R.color.colorAccent))
+         imageHappy.setColorFilter(resources.getColor(R.color.colorAccent))
+         imageMorning.setColorFilter(resources.getColor(R.color.colorAccent))
+
+         when (id){
+             R.id.imageAll ->{
+                 imageAll.setColorFilter(resources.getColor(R.color.white))
+             }
+             R.id.imageHappy ->{
+                 imageHappy.setColorFilter(resources.getColor(R.color.white))
+             }
+             R.id.imageMorning ->{
+                imageMorning.setColorFilter(resources.getColor(R.color.white))
+             }
+         }
+
+     }
+ }
